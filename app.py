b3d9922836804326e0555add2bb8d36dbf76e231
Alessandro-cesf <116165893+kouoji@users.noreply.github.com>
2024-04-12 08:22:11 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
-print("Programa Expresso/m")
-print("Cadastrar Restaurante")
-print("Listar restaurante")
-print("ativar restaurante")
-print("abrir/n")
+import os
 
+def finalizar_app():
+    os.system("cls")
+    print("Finalizando o app\n")
 
-opcao_digitada=input
-print("você escolheu:", opcao_digitada)
+def escolher_opcoes():
+    print("Programa Expresso")
+    print("*****************")
+    print("1 - Cadastrar restaurante")
+    print("2 - Listar restaurante")
+    print("3 - Ativar restaurante")
+    print("4 - Sair\n")
 
+def chamar_nome_do_app():
+    print("Restaurante Expressao\n")
 
-opcao_digitada=input
-print("você escolheu:", opcao_digitada)
+def main():
+    escolher_opcoes()
+    chamar_nome_do_app()
+    opcaodigitada = input("Digite a opção desejada: ")
 
-if(opcao_digitada==1):
-    print("você escolheu cadastrar um restaurante\n")
-elif (opcao_digitada==2):
-    print("você escolheu listar os restaurantes do app\n")
-elif(opcao_digitada==3):
-    print("você escolheu ativar um restaurante\n")
-elif(opcao_digitada==4):
-    print("você escolhey sair do programa\n")
+    if opcaodigitada == '1':
+        print("Você escolheu cadastrar restaurante")
+    elif opcaodigitada == '2':
+        print("Você escolheu listar restaurante")
+    elif opcaodigitada == '3':
+        print("Você escolheu ativar restaurante")
+    elif opcaodigitada == '4':
+        print("Você escolheu sair do aplicativo")
+    else:
+        print("Você escolheu sair do app")
+
+if __name__ == "__main__":
+    finalizar_app()
+    main()
